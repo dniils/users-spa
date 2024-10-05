190c6f7939277fc26af954e66e104d8a4d98b23a
dniils <tochka098@gmail.com>
2024-10-05 20:16:01 +0300

    feat: use generated images if email has no matching gravatar image

--- src/api/getImageSource.ts
+++ src/api/getImageSource.ts
@@ -4,5 +4,5 @@ const IMAGE_SIZE = 300;
 
 export function getImageSrc(email: string): string {
   const hash = hashEmail(email);
-  return `https://gravatar.com/avatar/${hash}?s=${IMAGE_SIZE}`;
+  return `https://gravatar.com/avatar/${hash}?s=${IMAGE_SIZE}&d=retro`;
 }
