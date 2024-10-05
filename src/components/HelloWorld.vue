b466c709a9895fa444223c56f636726b7fd44d5e
dniils <tochka098@gmail.com>
2024-10-05 12:48:06 +0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1
+    class="text-5xl font-bold text-teal-900 hover:text-teal-700 hover:translate-x-7 transition-all"
+  >
+    {{ msg }}
+  </h1>
+</template>
+
+<style scoped></style>
