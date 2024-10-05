de3f27443f3941f85318008563e1c132646f05a4
dniils <tochka098@gmail.com>
2024-10-05 22:57:15 +0300

    feat: add filtration by user name

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,30 @@ import { fetchUsers } from "./api/fetchUsers";
 import { getImageSrc } from "./api/getImageSource";
 
 const users = ref<User[]>([]);
+const usersToDisplay = ref<User[]>([]);
+const inputValue = ref<string>("");
 
 async function getUsers(): Promise<void> {
   const data = await fetchUsers();
   if (data) users.value = data;
 
-  users.value.map(async (user) => {
+  users.value.forEach(async (user) => {
     user.imageSource = getImageSrc(user.email);
   });
+
+  usersToDisplay.value = users.value;
+}
+
+function inputEventHandler() {
+  usersToDisplay.value = users.value;
+
+  if (inputValue.value.trim())
+    usersToDisplay.value = users.value.filter((user) =>
+      user.name
+        .trim()
+        .toLowerCase()
+        .includes(inputValue.value.trim().toLocaleLowerCase())
+    );
 }
 
 onMounted(() => {
@@ -24,10 +40,19 @@ onMounted(() => {
 <template>
   <div class="container mx-auto px-4">
     <h1 class="text-2xl font-bold text-center mb-6">User Cards</h1>
+
+    <input
+      type="text"
+      class="flex mx-auto w-full sm:w-1/2 lg:w-1/3 px-4 py-2 border border-slate-300 rounded-full mb-5"
+      placeholder="Start typing name..."
+      v-model.trim="inputValue"
+      @input="inputEventHandler"
+    />
+
     <div
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
     >
-      <UserCard v-for="user in users" :key="user.id" :user="user" />
+      <UserCard v-for="user in usersToDisplay" :key="user.id" :user="user" />
     </div>
   </div>
 </template>
