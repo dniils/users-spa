a12da86985303794597ecc3e028dce7382b42454
dniils <tochka098@gmail.com>
2024-10-08 12:16:30 +0300

    feat: add skeleton loader for images

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 defineProps(["user"]);
+const isLoading = ref(true);
+
+function handleImageLoad() {
+  isLoading.value = false;
+}
 </script>
 
 <template>
   <div class="w-full shadow rounded-md overflow-hidden flex flex-col">
-    <img :src="user.imageSource" alt="image" class="w-full h-64 object-cover" />
+    <div
+      v-if="isLoading"
+      class="w-full h-64 sm:size-64 bg-slate-200 animate-pulse"
+    ></div>
+    <img
+      v-show="!isLoading"
+      :src="user.imageSource"
+      alt="image"
+      class="w-full h-64 object-cover"
+      @load="handleImageLoad"
+    />
 
     <div class="p-2 flex-grow flex flex-col justify-between">
       <h2 class="font-bold text-center">{{ user.name }}</h2>
